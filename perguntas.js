0370807dcfca3bde411c6a11b23cdb5ed9c785e6
kemmerichbruno <kemmerich.bruno@escola.pr.gov.br>
2025-07-06 22:35:51 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Carro',
+    ' Para que serve o catalisador do carro?',
+    'Para diminuir a poluição, convertendo gases tóxicos em menos poluentes antes de saírem pelo escapamento.'
+)
+
+criaCartao(
+    'Moto',
+    'O que diferencia uma moto 2 tempos da 4 tempos?',
+    'A moto 2 tempos é mais leve e potente, mas consome mais combustível e polui mais. A 4 tempos é mais econômica, durável e menos poluente.'
+)
+
+criaCartao(
+    'Bicicleta',
+    ' Para que servem as marchas da bicicleta?',
+    'Para ajustar a força necessária ao pedalar, facilitando subidas com marchas leves e aumentando a velocidade em terrenos planos com marchas pesadas.'
+)
+
+criaCartao(
+    'Caminhão',
+    'Para que serve o freio motor em caminhões?',
+    ' Para ajudar a reduzir a velocidade usando o motor, preservando os freios e dando mais segurança em descidas.'
+)
